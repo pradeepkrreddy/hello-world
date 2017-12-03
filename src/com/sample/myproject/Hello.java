7e870d516f35f1c2266cf051d59d20a3ec52fa5e
Pradeep <pradeep.aarya@gmail.com>
2017-12-03 22:55:55 +0530

    Project skeleton
    First commit with project folder and source code

---
+++ src/com/sample/myproject/Hello.java
@@ -0,0 +1,17 @@
+package com.sample.myproject;
+/**
+ * @author pkr
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World, This is my first git-hub project");
+
+	}
+
+}
